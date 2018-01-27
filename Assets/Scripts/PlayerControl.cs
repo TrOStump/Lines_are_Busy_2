d134013fcacddb701d87b79010005a5bbd58e80e
TrOStump <TrOates97@gmail.com>
2018-01-27 01:05:08 -0500

    init commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Vector3 mousePos;
+    Camera mainCam;
+    Vector3 screenPoint;
+    Vector3 scanPos;
+    Vector3 offset;
+
+    private void Start()
+    {
+        mainCam = GetComponent<Camera>();
+    }
+    private void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        mousePos = mainCam.ScreenToWorldPoint(mousePos);
+    }
+}
