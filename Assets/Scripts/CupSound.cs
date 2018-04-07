31b04258be82087c5e24f51393b20800123c24f6
Allan <>
2018-04-06 22:10:11 -0400

    PrePreAlpha NO SERIOUSLY IT'S FINE NOW
    (title)

--- Assets/Scripts/CupSound.cs
+++ Assets/Scripts/CupSound.cs
@@ -8,6 +8,7 @@ public class CupSound : MonoBehaviour {
 	public bool moved = false;
 	public int random = 0;
 	int arbit = 750;
+	public int counter = 0;
 	public AudioClip cupMove;
 	// Use this for initialization
 	void Start () {
@@ -16,19 +17,22 @@ public class CupSound : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		random = (int)(Random.Range(0f, 1500f));
-		if((random==arbit)&&(moved==false))
-		{
-			gameObject.GetComponent<AudioSource>().PlayOneShot(cupMove);
-			moving = true;
-		}
-		if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 4.875)) {
-			gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.05f, 0f, 0f));
-		} else if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 5)) {
-			gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.02f, 0f, 0f));
-		} else if (gameObject.GetComponent<Transform> ().position.x > 5) {
-			moving = false;
-			moved = true;
+		if (counter <= 2000)
+			counter++;
+		else {
+			random = (int)(Random.Range (0f, 1500f));
+			if ((random == arbit) && (moved == false)) {
+				gameObject.GetComponent<AudioSource> ().PlayOneShot (cupMove);
+				moving = true;
+			}
+			if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 4.875)) {
+				gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.05f, 0f, 0f));
+			} else if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 5)) {
+				gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.02f, 0f, 0f));
+			} else if (gameObject.GetComponent<Transform> ().position.x > 5) {
+				moving = false;
+				moved = true;
+			}
 		}
 	}
 }
