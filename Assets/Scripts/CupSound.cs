3b3aedf06781f5f4ba9e05382e187f30ed929671
Allan <>
2018-04-18 01:56:48 -0400

    Quality of Life and Bug Removal
    Trevor, when you changed the size of everything you managed to somehow unhinge a bunch of stuff. I added pseudocode to the cup spoop, but also fixed a few unity-editor issues involving connections between certain objects and certain scripts. (For some reason the phone and the dialogue box were unhinged from things they needed to be attached to, coding-wise, but they should be good now).

--- Assets/Scripts/CupSound.cs
+++ Assets/Scripts/CupSound.cs
@@ -17,6 +17,23 @@ public class CupSound : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/***************
+		 * 
+		 * Pseudocode:
+		 * 	Wait a certain amount of time so that the scare doesn't happen immediately.
+		 * 	After timer:
+		 * 		Randomize a number.
+		 * 		If that number is equal to an arbitrary value, and the cup has not finished moving yet,
+		 * 			Make the movement noise and the cup is now moving.
+		 * 		If the cup is moving but has not finished moving,
+		 * 			and If the cup hasn't moved past (arbitrary x position)
+		 * 				Move the cup slightly towards (arbitrary x position)
+		 * 			or If the cup hasn't moved past (farther x position)
+		 * 				Move the cup even more slightly towards (farther x position) for an ease-in effect
+		 * 			or If the cup has moved past (farther x position)
+		 * 				The cup has finished moving
+		 * 
+		 ***************/
 		if (counter <= 2000)
 			counter++;
 		else {
@@ -25,13 +42,15 @@ public class CupSound : MonoBehaviour {
 				gameObject.GetComponent<AudioSource> ().PlayOneShot (cupMove);
 				moving = true;
 			}
-			if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 4.875)) {
-				gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.05f, 0f, 0f));
-			} else if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 5)) {
-				gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.02f, 0f, 0f));
-			} else if (gameObject.GetComponent<Transform> ().position.x > 5) {
-				moving = false;
-				moved = true;
+			if ((moving == true) && (moved == false)) {
+				if(gameObject.GetComponent<Transform> ().position.x < 5.88)
+					gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.05f, 0f, 0f));
+				else if ((moving == true) && (moved == false) && (gameObject.GetComponent<Transform> ().position.x < 5.98))
+					gameObject.GetComponent<Transform> ().Translate (new Vector3 (0.02f, 0f, 0f));
+				else if (gameObject.GetComponent<Transform> ().position.x > 5.98) {
+					moving = false;
+					moved = true;
+				} 
 			}
 		}
 	}
